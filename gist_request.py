7012bb65a732b6d94a309e3dfa57c4f631db150f
Nikewade <nikewade1006@gmail.com>
2023-09-13 19:18:20 -0400

    gist hosted song list setting up files to run 24/7 on replit

---
+++ gist_request.py
@@ -0,0 +1,46 @@
+import requests
+import sys
+
+gist_id = "6a8f32105497119045aa9da583bf919d"
+filename = "gistfile1.txt"
+songs_dict = {}
+
+def fetch_loop():
+    raw_url = fetch_gist_raw_url(gist_id, filename)
+    if raw_url is not None:
+        gist_content = fetch_gist_content(raw_url)
+        if gist_content is not None:
+            parse_content(gist_content)
+    return songs_dict
+
+def fetch_gist_raw_url(gist_id, filename):
+    gist_api_url = f"https://api.github.com/gists/{gist_id}"
+    response = requests.get(gist_api_url)
+    if response.status_code == 200:
+        gist_data = response.json()
+        if filename in gist_data['files']:
+            raw_url = gist_data['files'][filename]['raw_url']
+            return raw_url
+    print("Failed to fetch Gist raw URL.")
+    return None
+
+def fetch_gist_content(raw_url):
+    response = requests.get(raw_url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        print("Failed to fetch Gist content.")
+        return None
+
+def parse_content(content):
+    lines = content.strip().split('\n')
+    for line in lines:
+        key, value = map(str.strip, line.split('='))
+        songs_dict[key] = int(value)
+    return songs_dict
+
+def main():
+    fetch_loop()
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,66 +1,82 @@
 import spotipy
 from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyOAuth
-from subprocess import Popen
+from spotipy.oauth2 import SpotifyOAuth, CacheFileHandler
+import gist_request
 import os
 import time
+import threading
 
-starttime = time.time()
-stopTime = 1
-sp = None
-process = None
-songs = {}
+songs_dict = {}
 
-with open(os.getcwd() + "\data.txt") as f:
-    songs = dict(s.lower().replace(" ", "").strip().split("=") for s in f)
-    print(songs)
+def load_songs_from_file(file_path):
+    songs = {}
+    with open(file_path) as f:
+        songs = dict(s.lower().replace(" ", "").strip().split("=") for s in f)
+    return songs
 
-
-def initialize():
-    global sp
+def initialize_spotify():
     load_dotenv()
     CLIENT_ID = os.getenv("CLIENT_ID")
     CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-    REDIRECT_URI = "http://localhost:8888/callback"
+    REDIRECT_URI = "http://0.0.0.0:8888/callback"
     SCOPE = "user-modify-playback-state", "user-read-playback-state"
 
+    return spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=CLIENT_ID,
+            cache_handler=CacheFileHandler(cache_path="t.cache"),
+            client_secret=CLIENT_SECRET,
+            redirect_uri=REDIRECT_URI,
+            scope=SCOPE
+        )
+    )
 
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
-                                                client_secret=CLIENT_SECRET,
-                                                redirect_uri=REDIRECT_URI,
-                                                scope=SCOPE))
-    print("Init Done")
+def get_songs_loop():
+    while True:
+        print(gist_request.fetch_loop())
+        time.sleep(10)
 
-def open_spotify():
-    global process
-    spotify_path = r"C:\Users\nikew\AppData\Roaming\Spotify\Spotify.exe"
-    process = Popen([spotify_path])
-    check_song_loop()
-
-def check_song_loop():
-    while process.poll() == None:
+def check_song_loop(songs, sp):
+    while True:
         current_track = sp.current_user_playing_track()
-        if current_track is not None and current_track["is_playing"]:
-            current_song_name = str(current_track["item"]["name"]).replace(" ", "").lower()
 
-            current_time_ms = current_track["progress_ms"]
-            current_time_seconds = int(current_time_ms / 1000)
+        if current_track is None:
+            print("No track loaded.")
+            time.sleep(1)
+            continue
+        
+        if not current_track.get("is_playing", False):
+            print("Track is paused or stopped.")
+            time.sleep(1)
+            continue
+        
+        current_song_info = current_track.get("item")
+        
+        if current_song_info is None or "name" not in current_song_info:
+            print("No song information available.")
+            time.sleep(1)
+            continue
+        
+        current_song_name = str(current_song_info["name"]).replace(" ", "").lower()
+        current_time_ms = current_track["progress_ms"]
+        current_time_seconds = int(current_time_ms / 1000)
 
-            print("Current Song:", current_song_name)
-            print("Current Time (seconds):", current_time_seconds)
+        print("Current Song:", current_song_name)
+        print("Current Time (seconds):", current_time_seconds)
 
-            if(current_song_name in songs):
-                print("Song Name: " + current_song_name + " is in the list!")
-                print("Timestamp to cancel on: " + songs.get(current_song_name))
-                if(current_time_seconds >= int(songs.get(current_song_name))):
-                    sp.next_track()
-                    
-        else:
-            print("No track is currently playing.")
+        if current_song_name in songs:
+            print("Song Name:", current_song_name, "is in the list!")
+            print("Timestamp to cancel on:", songs.get(current_song_name))
+            if current_time_seconds >= int(songs.get(current_song_name)):
+                sp.next_track()
+        
         time.sleep(1)
 
+def main():
+    print("Loading song list...")
+    threading.Thread(target=get_songs_loop).start()
+    sp = initialize_spotify()
+    threading.Thread(target=check_song_loop(songs_dict, sp)).start()
 
-initialize()
-open_spotify()
-
-
+if __name__ == "__main__":
+    main()
